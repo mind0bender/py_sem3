92881e9bcc57e9551a8d2b23a77402c15a923bd2
mind0bender <mind0bender@gmail.com>
2024-07-18 15:44:25 +0530

    { "C": ["Bitwise Operator"] }

--- practical1/practical1.4.py
+++ practical1/practical1.4.py
@@ -5,10 +5,20 @@ b = int(input("Enter second number: "))
 c = int(input("Enter third number: "))
 
 if a > b and a > c:
-    print("{a} is greater".format(a=a))
+    print("{a} is greatest".format(a=a))
+elif b > c:
+    print("{b} is greatest".format(b=b))
+else:
+    print("{c} is greatest".format(c=c))
+
+if a > b or a > c:
+    print("{a} is might be greatest".format(a=a))
 elif b > c:
     print("{b} is greater".format(b=b))
 else:
     print("{c} is greater".format(c=c))
+
+print("not True is", not True)
+print("not False is", not False)
 
 print("\nYash Gupta\t23BCS11317")
---
+++ practical1/practical1.5.py
@@ -0,0 +1,13 @@
+# Bitwise Operators
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("{a} & {b} is {c}".format(a=a, b=b, c=a & b))
+print("{a} | {b} is {c}".format(a=a, b=b, c=a | b))
+print("{a} ^ {b} is {c}".format(a=a, b=b, c=a ^ b))
+print("~{a} is {b}".format(a=a, b=b, c=~a))
+print("{a} << {b} is {c}".format(a=a, b=b, c=a << b))
+print("{a} >> {b} is {c}".format(a=a, b=b, c=a >> b))
+
+print("\nYash Gupta\t23BCS11317")
