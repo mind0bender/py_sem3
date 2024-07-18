33c4e74411232275a5143694233d2fe71ef86447
mind0bender <mind0bender@gmail.com>
2024-07-18 15:01:40 +0530

    { "C": ["Arithmetic Operators"] }

---
+++ practical1/practical1.1.py
@@ -0,0 +1,10 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("The sum of %d and %d is %d" % (a, b, a + b))
+print("The difference of %d and %d is %d" % (a, b, a - b))
+print("The product of %d and %d is %d" % (a, b, a * b))
+print("The division of %d and %d is %f" % (a, b, a / b))
+print("The exponent of %d and %d is %d" % (a, b, a**b))
+print("The mod of %d and %d is %d" % (a, b, a % b))
+print("The floor of %d and %d is %d" % (a, b, a // b))
